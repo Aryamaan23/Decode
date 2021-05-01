f3258fea0b64e5e0236ad478b1049bb4ccf8374a
Aryamaan <pandeyaryamaan@gmail.com>
2021-05-01 09:45:29 +0530

    feat: add routers in app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -11,6 +11,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(CodeCompileRouter, tags=["Code Compile"], prefix="/api/v1/compile")
+app.include_router(ConvertCodeIntoPseudoRouter, tags=["Convert Code into Pseudo Code"], prefix="/api/v1/convert")
+app.include_router(TranslateRouter, tags=["Translate Pseudo Code"], prefix="/api/v1/translate")
+
 
 @app.get("/", tags=["Root"])
 async def read_root():
