b262f9540ff4a619f794cd70a07d828f6f1c0dc7
Aryamaan <pandeyaryamaan@gmail.com>
2021-05-01 09:33:07 +0530

    feat: convert code into pseudocode

---
+++ backend/app/routes/convertPseudo.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Body, Form
+from fastapi.encoders import jsonable_encoder
+
+from ..services.pseudo_code import convertCodeIntoPseudoCode
+
+router = APIRouter()
+
+
+@router.post("/", response_description="Convert Python Code into Pseudo Code")
+async def compile(source: str = Form(...)):
+    return convertCodeIntoPseudoCode(source)
