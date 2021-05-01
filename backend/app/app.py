83d28e5a529afe47f705ebca232649bf94fafe6e
Ankit Hans <ankithans1947@gmail.com>
2021-05-01 08:50:04 +0530

    feat: init fastapi

---
+++ backend/app/app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to Decode"}
